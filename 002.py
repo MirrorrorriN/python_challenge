9bba2797dbb0ad8aadbdfb6e809f3da282de5092
MirrorrorriN <MirrorrorriN@163.com>
2017-07-25 13:21:52 +0800

    new challenge

--- 002.py
+++ 002.py
@@ -10,17 +10,3 @@ if __name__=="__main__":
     for c in text: counts[c] = counts.get(c, 0) + 1
     print counts
     print ''.join(re.findall('[a-z]', text))
-
-# import urllib
-# import re
-# def get_pythonchallenge(url):
-# 	return urllib.urlopen(url).read()
-
-# if __name__=="__main__":
-# 	src = get_pythonchallenge('http://www.pythonchallenge.com/pc/def/ocr.html')
-# 	text = re.compile('<!--(.*)-->',re.S).findall(src)[-1]
-# 	counts = {}
-# 	for item in text:
-# 		counts[item] = counts.get(item,0)+1
-# 	print counts
-# 	print ''.join(re.findall('[a-z]',text))
